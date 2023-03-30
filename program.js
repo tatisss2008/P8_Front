462e58231f33d75a74977b07b1ce4e30bc7558f3
AnaM <test>
2023-03-30 14:28:29 -0500

    v3

--- program.js
+++ program.js
@@ -3,7 +3,8 @@
 
 const miBotonS = document.getElementById("submitS");
 const miBotonR = document.getElementById("submitR");
-
+const miBotonM = document.getElementById("submitM");
+const miBotonD = document.getElementById("submitD");
 
 miBotonS.addEventListener(
     'click',
@@ -41,6 +42,58 @@ miBotonR.addEventListener(
 
         const respuesta= await fetch(
             'http://localhost:3000/api/restar',
+            {
+                "method":"POST",
+                "headers":{
+                    "Content-Type":"application/json"
+                },
+                "body":JSON.stringify({numero_1,numero_2})
+            }
+        );
+
+        const dato=await respuesta.json();
+        const div_resultado=document.getElementById("resultado");
+        div_resultado.innerHTML=dato;
+    }
+);
+
+
+miBotonM.addEventListener(
+    'click',
+    async (event)=>{
+        event.preventDefault();
+
+        const numero_1=parseFloat(document.getElementById("num1").value);
+        const numero_2=parseFloat(document.getElementById("num2").value);        
+
+        const respuesta= await fetch(
+            'http://localhost:3000/api/multiplicar',
+            {
+                "method":"POST",
+                "headers":{
+                    "Content-Type":"application/json"
+                },
+                "body":JSON.stringify({numero_1,numero_2})
+            }
+        );
+
+        const dato=await respuesta.json();
+        const div_resultado=document.getElementById("resultado");
+        div_resultado.innerHTML=dato;
+    }
+);
+
+
+miBotonD.addEventListener(
+    'click',
+    async (event)=>{
+        event.preventDefault();
+
+        const numero_1=parseFloat(document.getElementById("num1").value);
+        const numero_2=parseFloat(document.getElementById("num2").value);        
+
+        const respuesta= await fetch(
+            'http://localhost:3000/api/dividir',
             {
                 "method":"POST",
                 "headers":{
